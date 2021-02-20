00565bdd9b28fa0282f0e521e0b11a584e89a276
David <angel.santa.0398@gmail.com>
2021-02-20 14:02:49 -0500

    initial

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    /// <summary>
+    /// First Console App lecture code
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Prints a message
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) 
+        {
+            //print supportive message
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
