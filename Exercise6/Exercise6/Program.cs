d07754b23aa15e8cc61546f1de12337cd5e2a71b
David <angel.santa.0398@gmail.com>
2021-02-20 21:49:02 -0500

    Exercise6

--- Exercise6/Exercise6/Program.cs
+++ Exercise6/Exercise6/Program.cs
@@ -14,7 +14,7 @@ namespace Exercise6
             float angle = float.Parse(Console.ReadLine());
 
             float cosine = (float)Math.Cos(angle * Math.PI / 180);
-            float sine = (float)Math.Cos(angle * Math.PI / 180);
+            float sine = (float)Math.Sin(angle * Math.PI / 180);
 
             Console.WriteLine("The sine of " + angle + " degrees is: " + sine);
             Console.WriteLine("The cosine of " + angle + " degrees is: " + cosine);
