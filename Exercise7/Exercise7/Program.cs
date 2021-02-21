d7361e6ae93cbaa6acaad56bf6d6f6f8e8b0e50e
David <angel.santa.0398@gmail.com>
2021-02-20 22:32:50 -0500

    Exercise7

---
+++ Exercise7/Exercise7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Insert a Fahrenheit temperature: ");
+            float fahr = float.Parse(Console.ReadLine());
+
+            float celsius = (fahr - 32) / 9 * 5;
+
+            Console.WriteLine(fahr + " degrees fahrenheir equals to " + celsius + " degrees celsius");
+
+        }
+    }
+}
