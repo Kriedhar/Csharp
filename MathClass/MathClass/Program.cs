b67570c50f40127974a72a4d547fe7cfbe3e3d1f
David <angel.santa.0398@gmail.com>
2021-02-20 21:06:03 -0500

    MathClass

---
+++ MathClass/MathClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+    /// <summary>
+    /// Example of search documentation
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demostrates reading documentation
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // calculate and print cosine of 45 degrees
+            float cosine = (float)Math.Cos(45 * Math.PI / 180);
+            Console.WriteLine("Cosine of 45 degrees: " + cosine);
+
+            Console.WriteLine();
+
+        }
+    }
+}
